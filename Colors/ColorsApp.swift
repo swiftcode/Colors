a48227bba33be921ed2d7d221568c52309ea6dbd
swiftcode <swiftcode@users.noreply.github.com>
2025-01-26 11:44:25 -0600

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Michael Campbell on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
